bda3c2d861221e48b1738a7269696ee5b3b4fb38
chuquidev <jchuquipomajimenez@gmail.com>
2026-09-18 17:09:50 -0500

    nuevo cambio

--- web/src/views/SizeGuideView.vue
+++ web/src/views/SizeGuideView.vue
@@ -24,7 +24,7 @@ const rightRows = computed(() => {
 </script>
 
 <template>
-    <div class="mx-auto max-w-4xl px-4 py-6 sm:py-8">
+    <div class="mx-auto max-w-[1400px] px-4 py-6 sm:py-8">
         <Breadcrumbs :items="[{ label: 'Inicio', to: '/' }, { label: 'Guía de tallas' }]" />
 
         <h1 class="font-display text-2xl font-bold text-gray-900 dark:text-gray-100 sm:text-3xl">Guía de tallas</h1>
